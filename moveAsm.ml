43f66cd3d6fdd2810aacc2104500f0775847834e
wata <none@none>
2010-01-26 13:03:14 +0900

    スケジューラの準備

--- moveAsm.ml
+++ moveAsm.ml
@@ -20,23 +20,32 @@ let rec remove exp = function
   | Seq(exp', e) -> Seq(exp', remove exp e)
   | _ -> assert false
 
-let rec g cont = function
+let rec g = function
   | End | Ret _ | Jmp _ as e -> e
-  | Call(s, e) -> Call(s, g cont e)
-  | Seq(Exp(asm, _, _) as exp, e) -> Seq(exp, g cont e)
+  | Call(s, e) -> Call(s, g e)
+  | Seq(Exp(asm, _, _) as exp, e) -> Seq(exp, g e)
   | If(b, bn, e1, e2, e3) ->
-      let cont' = seq e3 cont in
       let exps = inter (getFirst ["cond"] [] e1) (getFirst ["cond"] [] e2) in
       if e1 = End && e2 = End then
-        ((*Format.eprintf "RemoveJmp@.";*) g cont e3)
+        ((*Format.eprintf "RemoveJmp@.";*) g e3)
       else if exps <> [] then
         let exp = List.hd exps in
-        ((*Format.eprintf "MoveFirst@.";*) Seq(exp, g cont (If(b, bn, remove exp e1, remove exp e2, e3))))
+        ((*Format.eprintf "MoveFirst@.";*) Seq(exp, g (If(b, bn, remove exp e1, remove exp e2, e3))))
       else
-        If(b, bn, g cont' e1, g cont' e2, g cont e3)
+        If(b, bn, g e1, g e2, g e3)
 
-let rec f (Prog(data, fundefs, e)) =
-  let fundefs' = List.map (fun (x, e) -> (x, g End e)) fundefs in
-  let e' = g End e in
-  if fundefs' = fundefs && e = e' then (Prog(data, fundefs, e))
-  else f (Prog(data, fundefs', e'))
+let rec schedule write = function
+  | End | Ret _ | Jmp _ as e -> e
+  | Call(s, e) -> Call(s, schedule [] e)
+  | If(b, bn, e1, e2, e3) -> If(b, bn, schedule [] e1, schedule [] e2, schedule [] e3)
+  | Seq(exp, e) -> Seq(exp, schedule [] e)
+
+let rec h e =
+  let e' = g e in
+  if e' = e then schedule [] e'
+  else h e
+
+let f (Prog(data, fundefs, e)) =
+  let fundefs = List.map (fun (f, e) -> (f, h e)) fundefs in
+  let e = h e in
+  Prog(data, fundefs, e)
