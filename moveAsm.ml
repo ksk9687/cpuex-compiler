4364ddbd2487edf0004ab4f0dde698d40a5db686
wata <none@none>
2010-01-27 20:08:57 +0900

    ちょっと修正

--- moveAsm.ml
+++ moveAsm.ml
@@ -10,7 +10,7 @@ let inter xs ys =
   List.fold_left (fun zs x -> if List.mem x ys then x :: zs else zs) [] xs
 
 let rec getFirst reads writes = function
-  | Seq(Exp(asm, read, write) as exp, e) when not (List.mem "all" write) ->
+  | Seq(Exp(asm, read, write) as exp, e) ->
       if (inter (reads @ writes) write) <> [] || (inter writes read) <> [] then getFirst (read@reads) (write@writes) e
       else exp :: (getFirst (read@reads) (write@writes) e)
   | _ -> []
@@ -35,25 +35,21 @@ let rec g = function
         If(b, bn, g e1, g e2, g e3)
 
 let aging write =
-  let write' = List.map (fun (x, y) -> (x-1, y)) write in
-    List.filter (fun (x, _) -> x >= 0) write'
+  let write' = List.map (fun (x, y) -> (x - 1, y)) write in
+  List.filter (fun (x, _) -> x >= 0) write'
+
+let miss = ref 0
 
 let rec schedule awrite = function
   | End | Ret _ | Jmp _ as e -> e
   | Call(s, e) -> Call(s, schedule [] e)
   | If(b, bn, e1, e2, e3) -> If(b, bn, schedule [] e1, schedule [] e2, schedule [] e3)
-  | Seq(Exp(asm, reads, writes) as exp, e) ->
+  | Seq(exp, e) as es ->
       let awrite = aging awrite in
-      let write = List.fold_left (fun x (_,y) -> y@x) [] awrite in
-	if write = [] then Seq(exp, schedule [(4,writes)] e) 
-	else 
-	  if inter write reads <> [] then
-	    match getFirst reads (writes@write) e with
-	      | [] -> Seq(exp, schedule ((4,writes)::awrite) e)
-	      | (Exp(_,_,w) as hd) :: _ -> Seq(hd, schedule ((4,w)::awrite) (Seq(exp, remove hd e)))
-	  else
-	    schedule awrite (Seq(exp, e))
-
+      let write = List.fold_left (fun x (_, y) -> y @ x) [] awrite in
+      let exps = getFirst [] write es in
+      let exp = if exps <> [] then List.hd exps else (incr miss; exp) in
+      Seq(exp, schedule ((4, (getWrite exp)) :: awrite) (remove exp es))
 
 let rec h e =
   let e' = g e in
@@ -62,5 +58,7 @@ let rec h e =
 
 let f (Prog(data, fundefs, e)) =
   let fundefs = List.map (fun (f, e) -> (f, h e)) fundefs in
+  miss := 0;
   let e = h e in
+  Format.eprintf "MissCount: %d@." !miss;
   Prog(data, fundefs, e)
